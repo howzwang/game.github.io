effaa9765461d3dbeac07a19e2c44852ef0537bb
Sam Wang <howz.wang@gmail.com>
2023-12-20 16:50:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-// Define the legs and shuffle function
-const legs = ['Leg 1', 'Leg 2', 'Leg 3', 'Leg 4']; // Add more legs if needed
+const legs = [
+    { thing: 'Prize 1' },
+    { thing: 'Prize 2' },
+    { thing: 'Prize 3' },
+    { thing: 'Prize 4' }
+];
 
 function shuffleLegs() {
-    const shuffledLegs = shuffleArray(legs.slice()); // Create a copy of the array to avoid modifying the original
+    const shuffledLegs = shuffleArray(legs.slice());
     displayLegs(shuffledLegs);
 }
 
-// Function to shuffle array elements
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -15,14 +18,25 @@ function shuffleArray(array) {
     return array;
 }
 
-// Function to display shuffled legs
 function displayLegs(legsArray) {
     const legsContainer = document.getElementById('legs-container');
     legsContainer.innerHTML = '';
 
     legsArray.forEach((leg, index) => {
-        const legDiv = document.createElement('div');
-        legDiv.textContent = `Person ${index + 1}: ${leg}`;
-        legsContainer.appendChild(legDiv);
+        const verticalLine = document.createElement('div');
+        verticalLine.className = 'vertical-line';
+
+        for (let i = 0; i < Math.floor(Math.random() * 5) + 1; i++) {
+            const horizontalLine = document.createElement('div');
+            horizontalLine.className = 'horizontal-line';
+            verticalLine.appendChild(horizontalLine);
+        }
+
+        const thingLabel = document.createElement('div');
+        thingLabel.className = 'thing-label';
+        thingLabel.textContent = leg.thing;
+        verticalLine.appendChild(thingLabel);
+
+        legsContainer.appendChild(verticalLine);
     });
 }
