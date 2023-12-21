732b487200af67122bf56f3220757c54ec7e6905
Sam Wang <howz.wang@gmail.com>
2023-12-21 11:10:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,46 @@
-const legs = [
-    { thing: 'Prize 1' },
-    { thing: 'Prize 2' },
-    { thing: 'Prize 3' },
-    { thing: 'Prize 4' }
+const pairs = [
+    { name: 'Alice', number: 1 },
+    { name: 'Bob', number: 2 },
+    { name: 'Charlie', number: 3 },
+    // Add more pairs as needed
 ];
 
-function shuffleLegs() {
-    const shuffledLegs = shuffleArray(legs.slice());
-    displayLegs(shuffledLegs);
-}
+const gameContainer = document.getElementById('game-container');
+let selectedCards = [];
 
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-}
-
-function displayLegs(legsArray) {
-    const legsContainer = document.getElementById('legs-container');
-    legsContainer.innerHTML = '';
-
-    legsArray.forEach((leg, index) => {
-        const verticalLine = document.createElement('div');
-        verticalLine.className = 'vertical-line';
-
-        for (let i = 0; i < Math.floor(Math.random() * 5) + 1; i++) {
-            const horizontalLine = document.createElement('div');
-            horizontalLine.className = 'horizontal-line';
-            verticalLine.appendChild(horizontalLine);
-        }
-
-        const thingLabel = document.createElement('div');
-        thingLabel.className = 'thing-label';
-        thingLabel.textContent = leg.thing;
-        verticalLine.appendChild(thingLabel);
-
-        legsContainer.appendChild(verticalLine);
+function createGameBoard() {
+    pairs.forEach(pair => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.textContent = '?'; // Display a question mark initially
+        card.addEventListener('click', () => flipCard(card, pair));
+        gameContainer.appendChild(card);
     });
 }
+
+function flipCard(card, pair) {
+    if (selectedCards.length < 2) {
+        card.textContent = pair.name;
+        selectedCards.push({ card, pair });
+
+        if (selectedCards.length === 2) {
+            setTimeout(checkMatch, 500);
+        }
+    }
+}
+
+function checkMatch() {
+    const [card1, card2] = selectedCards;
+
+    if (card1.pair.number === card2.pair.number) {
+        card1.card.classList.add('matched');
+        card2.card.classList.add('matched');
+    } else {
+        card1.card.textContent = '?';
+        card2.card.textContent = '?';
+    }
+
+    selectedCards = [];
+}
+
+window.addEventListener('DOMContentLoaded', createGameBoard);
